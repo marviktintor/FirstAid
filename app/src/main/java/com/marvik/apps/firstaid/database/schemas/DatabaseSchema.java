bf2f50daf9c716ce5cdc0703cc501e5fbb85250f
Victor Mwenda <vmwenda.vm@gmail.com>
2015-09-01 06:35:37 +0300

    Created Database SQL

--- app/src/main/java/com/marvik/apps/firstaid/database/schemas/DatabaseSchema.java
+++ app/src/main/java/com/marvik/apps/firstaid/database/schemas/DatabaseSchema.java
@@ -1,7 +1,24 @@
 package com.marvik.apps.firstaid.database.schemas;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by victor on 9/1/2015.
  */
-public class DatabaseSchema {
+public class DatabaseSchema extends SQLiteOpenHelper{
+    public DatabaseSchema(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
--- app/src/main/java/com/marvik/apps/firstaid/database/tables/Tables.java
+++ app/src/main/java/com/marvik/apps/firstaid/database/tables/Tables.java
@@ -5,26 +5,46 @@ package com.marvik.apps.firstaid.database.tables;
  */
 public class Tables {
     public static class Attacks {
+        public static final String TABLE_NAME = "tbl_attacks";
         public static final String COL_ATTACK_ID = "attack_id";
         public static final String COL_ATTACK = "attack";
-
+        public static final String SQL = "CREATE TABLE IF NOT EXISTS " +TABLE_NAME +"("
+                +COL_ATTACK_ID +" integer primary key autoincrement,"
+                +COL_ATTACK +" text not null);";
     }
 
     public static class Symptoms {
+        public static final String TABLE_NAME = "tbl_symptoms";
         public static final String COL_ATTACK_ID = "attack_id";
         public static final String COL_SYMPTOM_ID = "symptom_id";
         public static final String COL_SYMPTOM = "symptom";
+
+        public static final String SQL = "CREATE TABLE IF NOT EXISTS " +TABLE_NAME +"("
+                +COL_SYMPTOM_ID +" integer primary key autoincrement,"
+                +COL_SYMPTOM +" text not null,"
+                +COL_ATTACK_ID +" integer);";
+
     }
 
     public static class FirstAids {
+        public static final String TABLE_NAME = "tbl_firstaids";
         public static final String COL_ATTACK_ID = "attack_id";
         public static final String COL_FIRST_AID_ID = "first_aid_id";
         public static final String COL_FIRST_AID = "first_aid";
+        public static final String SQL = "CREATE TABLE IF NOT EXISTS " +TABLE_NAME +"("
+                +COL_FIRST_AID_ID +" integer primary key autoincrement,"
+                +COL_FIRST_AID +" text not null,"
+                +COL_ATTACK_ID +" integer);";
     }
 
     public static class Notes {
+        public static final String TABLE_NAME = "tbl_notes";
         public static final String COL_ATTACK_ID = "attack_id";
         public static final String COL_NOTE_ID = "note_id";
         public static final String COL_NOTE = "note";
+        public static final String SQL = "CREATE TABLE IF NOT EXISTS " +TABLE_NAME +"("
+                +COL_NOTE_ID +" integer primary key autoincrement,"
+                +COL_NOTE +" text not null,"
+                +COL_ATTACK_ID +" integer);";
     }
 }
