d6e12af5a2626e71db9386d367d8d444a4c02820
Victor Mwenda <vmwenda.vm@gmail.com>
2015-09-02 01:42:08 +0300

    Renamed File
    Renamed File

---
+++ app/src/main/java/com/marvik/apps/firstaid/activity/ActivityEmergencySupport.java
@@ -0,0 +1,54 @@
+package com.marvik.apps.firstaid.activity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.marvik.apps.firstaid.R;
+import com.marvik.apps.firstaid.utils.activities.ActivityWrapper;
+
+import java.util.ArrayList;
+
+/**
+ * Created by victor on 9/2/2015.
+ */
+public class ActivityEmergencySupport extends ActivityWrapper {
+    private ListView lvEmergencyAndSupport;
+
+    @Override
+    protected void onCreateActivity(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_emergency_and_support);
+        initChildViews();
+    }
+
+    private void initChildViews() {
+        lvEmergencyAndSupport = (ListView) findViewById(R.id.activity_emergency_and_support_listView_emergency_and_support);
+        ArrayList<String> emergencyAndSupport = new ArrayList<String>();
+
+
+        emergencyAndSupport.add("Purchase a first aid kit");
+        emergencyAndSupport.add("Get emergency medical unit");
+        emergencyAndSupport.add("Nearest hospital to you");
+        emergencyAndSupport.add("Ask a doctor");
+        emergencyAndSupport.add(" Report a medical issue");
+        emergencyAndSupport.add("Get check-up and testing");
+
+        lvEmergencyAndSupport.setAdapter(new ArrayAdapter<String>(getApplicationContext(), R.layout.list_emergency_and_support, R.id.list_emergency_and_support_textView_item, emergencyAndSupport));
+
+    }
+
+    @Override
+    protected void onResumeActivity() {
+
+    }
+
+    @Override
+    protected void onPauseActivity() {
+
+    }
+
+    @Override
+    protected void onDestroyActivity() {
+
+    }
+}
