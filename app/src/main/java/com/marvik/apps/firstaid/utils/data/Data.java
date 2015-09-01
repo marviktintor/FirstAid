398e8a5bda7e93cd59e699e0c2b16ad26765fe2f
Victor Mwenda <vmwenda.vm@gmail.com>
2015-09-02 01:22:03 +0300

    Fixed Typos

--- app/src/main/java/com/marvik/apps/firstaid/utils/data/Data.java
+++ app/src/main/java/com/marvik/apps/firstaid/utils/data/Data.java
@@ -4,9 +4,10 @@ package com.marvik.apps.firstaid.utils.data;
  * Created by victor on 9/1/2015.
  */
 public class Data {
+
         static public String[] attacks = {"Asthma Attack", "Burns", "Burns", "Burns", "Bone Fracture", "Chemical Burns - ON SKIN", "Chemical Burns - SWALLOWED",
                 "Cuts and Wounds - MAJOR", "Cuts and Wounds - MINOR", "Drowning", "Electric Burns", "Heart Attack/Arrest", "Hypothermia", "Hiccups",
-                "Snake Bites", "Stings", "Attention", "Stroke", "Pulse", "CPR - (Cardiopulmonary resuscitation) - Adult", "CPR - (Cardiopulmonary resuscitation) - Child"};
+                "Snake Bites", "Stings", "Attention", "Stroke", "Pulse - Radial- Radial pulse (inside the wrist)","Pulse - Carotid pulse (on the neck)","Recovery position", "CPR - (Cardiopulmonary resuscitation) - Adult", "CPR - (Cardiopulmonary resuscitation) - Child"};
         static public String[] symptoms = {
                 "Coughing,Wheezing (whistling sound when breathing),Shortness of breath,Chest tightness,Inability to talk (this varies with the magnitude of the attack)",//Asthma Attack
                 "Pain,Reddening of the skin,Swelling",//Burns
@@ -27,6 +28,8 @@ public class Data {
                 "Breathing problems,Abdominal cramps,Swelling of lips face or throat,Nausea or vomiting,Hives,Shock",//Attention
                 "",//Stroke
                 "",//Pulse
+                "",//Pulse
+                "",//Recovery position
                 "",//CPR - ADULT
                 "",//CPR - CHILD
         };
@@ -44,13 +47,15 @@ public class Data {
                 "Wear protective gloves,Stop bleeding by applying pressure to the wound with a clean cloth or gauze,Thorough rinse with water and alcohol (methylated spirit),Uses soap to clean around the wound and use tweezer to remove debris,Apply antibiotic cream to keep moist.,Cover the wound with bandage to keep harmful bacteria out,See a doctor for stitching,Change bandage at least once a day and when wet or dirty,If you identify an infection, see a doctor",//Cuts and Wounds - MAJOR
                 "Call the lifeguard or emergency response unit,Pull the victim out of the water,Check breathing by putting your ear close to the mouth,If not breathing check pulse,If no pulse begin CPR,Repeat CPR if the person is not breathing",//Drowning
                 "LOW SUPPLY OF POWER, e.g., domestic usage,Switch off the power,Remove the causality from the source with non-conductive material (wood),If the causality is unconscious conduct CPR,HIGH SUPPLY OF POWER,Call emergency medical unit",//Electric Burns
-                "Sit the person down and try keeping them calm,Loosen tight clothing especially coats and shirts,Give chest pain medication( if person is prescribed, i.e. nitro-glycerine),If symptoms persist or person is unconscious call emergency medical unit immediately,Conduct CPR.",//Heart Attack/Arrest
+                "Sit the person down and try keeping them calm,Loosen tight clothing especially coats and shirts,Give chest pain medication( if person is prescribed, i.e. nitro-glycerine)If symptoms persist or person is unconscious call emergency medical unit immediately Conduct CPR.",//Heart Attack/Arrest
                 "Call emergency medical unit,Take the person indoor,Change wet clothes to dry ones,Add blankets to facilitate more warmth especially on the neck and head,Give the person warm fluids,If unconscious begin CPR until help arrives",//Hypothermia
                 "Hold and take deep breathes by counting 1-10,Drink a glass of water completely without prose,Put a spoonful of sugar or honey on the tongue,If the hiccups persist see a medical doctor",//Hiccups
                 "Call the emergency medical unit immediately,Ensure that the bitten area is below the heart level to reduce spread of venom,Remove constricting items such as clothing jewellery the area may swell,Transport the victim by carrying or vehicle DO NOT MAKE THEM WALK,Take the snake with you for easy identification of the antidote",//Snake Bites
                 "Remove the stinger using forceps or scrap away using finger nails,Wash the area with soap and water,Apply a cold compressing using cloth dipped in cold water or filled with ice,Elevate the sting (if on the arms and legs),Apply gel cream (calamine) or baking soda to reduce itching,Take over the counter pain relievers.",//Stings
                 "Call Emergency medical unit, Ask if the person if is carrying an epinephrine injector,Keep the person calm warm and loosen tight clothing,If the person is vomiting turn them to the side (recovery position),If the person is unconscious begin CPR until help arrives",//Attention
                 " Numbness or weakness in the face arms legs especially one side,Blurred vision or blindness (an eye or both),Difficulty speaking or understanding,Dizziness,Severe headache especially with no cause,Loss of consciousness",//Stroke
+                "Stretch the arm and place the pads of the two fingers at the base of the thumb,Press lightly and move fingers until you feel a pulse", // - Pulse
+                "Place the pads of the middle and index finger between the windpipe and the large muscles of the neck,Press lightly until you feel a pulse", // Pulse
                 "Lay the victim on the floor,Kneel beside them and put the hand nearest to you at a right angle,Take the other hand and gently place under the chin nearest to you,Bend the knee further from you,Pull the knee toward you to roll the victim while your hand support the one under the chin,Move the leg to the floor and raise their chin to tilt head slightly,Check for obstruction",//Recovery position
                 "Lay the victim on the floor facing up,Tilt the head back and lift the chin and check for any obstructions,Pinch nose and cover the mouth with yours and blow until the chest rises,Give two breaths for two second,Push using two hands in the centre of the chest 2 inches 30 times,Give 30 pumps and two breaths until help arrives ",//CPR - ADULT
                 "Lay the victim on the floor facing up,Tilt the head back and lift the chin and check for any obstructions,Pinch nose and cover the mouth with yours and blow gently until the chest rises,Give two breaths for two second,Push using one hand in the centre of the chest 2 inches 30 times,Give 30 pumps and two breaths until help arrives",//CPR - CHILD
@@ -76,7 +81,9 @@ public class Data {
                 "",//Stings
                 "",//Attention
                 "Call emergency medical unit,Give first aid for cuts and wounds if any,If unconscious begin CPR ,Lay the victim in a recovery position,Monitor until help arrives",//Stroke
-                "Do not use the thumb,Use the middle and the index finger,Radial pulse (inside the wrist),Stretch the arm and place the pads of the two fingers at the base of the thumb,Press lightly and move fingers until you feel a pulse,Carotid pulse (on the neck),Place the pads of the middle and index finger between the windpipe and the large muscles of the neck,Press lightly until you feel a pulse",//Pulse
+                "Do not use the thumb,Use the middle and the index finger",//Pulse - Radial
+                "Do not use the thumb,Use the middle and the index finger",//Pulse - Carotoid
+                "",
                 "",//CPR - ADULT
                 "",//CPR - CHILD
         };
