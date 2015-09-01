f971545cf014aeeab235de6d9b2469fb74b0a814
Victor Mwenda <vmwenda.vm@gmail.com>
2015-09-01 13:34:17 +0300

    Dirtily working on inserting ALL data to database

--- app/src/main/java/com/marvik/apps/firstaid/utils/utililities/Utils.java
+++ app/src/main/java/com/marvik/apps/firstaid/utils/utililities/Utils.java
@@ -1,9 +1,11 @@
 package com.marvik.apps.firstaid.utils.utililities;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
+import com.marvik.apps.firstaid.database.tables.Tables;
 import com.marvik.apps.firstaid.database.transactions.TransactionsManager;
 import com.marvik.apps.firstaid.utils.prefs.Prefs;
 
@@ -33,6 +35,26 @@ public class Utils {
     }
 
     public void initAttacksFirstAids() {
+        String [] attacks = {"","Asthma Attack","Burns","Burns","Burns","Bone Fracture","Chemical Burns","Cuts and Wounds","Drowning",
+                "Electric Burns","Heart Attack/Arrest","Hypothermia","Hiccups","Snake Bites","Stings","Attention","Stroke","Pulse","CPR"};
+
+        ContentValues values = null;
+        int degree = 0;
+        for(int i = 1;i < attacks.length; i++){
+
+            values = new ContentValues();
+            values.put(Tables.Attacks.COL_ATTACK,attacks[i]);
+            if(i > 1 && i < 5) {
+                degree++;
+                values.put(Tables.Attacks.COL_DEGREE, degree);
+            }
+            getTransactionsManager().getSqLiteDatabase().insert(Tables.Attacks.TABLE_NAME,null,values);
+        }
+       Asthma-> Coughing,Wheezing (whistling sound when breathing),Shortness of breath,Chest tightness,Inability to talk (this varies with the magnitude of the attack)
+        Sit/make the victim sit upright,Ensure a comfortable position by raising the hands forward,Breathe in deeply by exhaling sharply,Do this in multiple rounds until normal breathing,After 30 minutes use your/their medicated inhaler
+        NOTE: If the victim cannot talk, call emergency medical unit first.
+
+
 
 
 
@@ -43,5 +65,10 @@ public class Utils {
         String [] symptoms = {};
         String [] firstAids = {};
         String [] notes = {};
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+        SharedPreferences.Editor editor=  prefs.edit();
+        editor.putBoolean(Prefs.FIRSTRUN,false);
+        editor.commit();
     }
 }
