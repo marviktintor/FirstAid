4128554fa61e194e54dcb5f53a0d4484f6a4bfd8
Victor Mwenda <vmwenda.vm@gmail.com>
2015-09-01 06:38:19 +0300

    Implemented Database SQL

--- app/src/main/java/com/marvik/apps/firstaid/database/schemas/DatabaseSchema.java
+++ app/src/main/java/com/marvik/apps/firstaid/database/schemas/DatabaseSchema.java
@@ -4,16 +4,26 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.marvik.apps.firstaid.database.tables.Tables;
+
 /**
  * Created by victor on 9/1/2015.
  */
-public class DatabaseSchema extends SQLiteOpenHelper{
-    public DatabaseSchema(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+public class DatabaseSchema extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "FirstAids";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseSchema(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL(Tables.Attacks.SQL);
+        db.execSQL(Tables.Symptoms.SQL);
+        db.execSQL(Tables.FirstAids.SQL);
+        db.execSQL(Tables.Notes.SQL);
 
     }
 
